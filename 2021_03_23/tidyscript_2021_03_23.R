626ddde6a124ccd66c6c13c9039d65fee80fccc1
dataGP-UK <sihulme21@gmail.com>
2021-03-25 15:51:48 +0000

    Installation of datasets amd packages

--- 2021_03_23/tidyscript_2021_03_23.R
+++ 2021_03_23/tidyscript_2021_03_23.R
@@ -1,3 +1,23 @@
-# 1st day of tidy tuesday
+# 25/3/2021 Simon Hulme dataGP-UK
 
-# 2nd line of code
+# tidy tuesday 23rd March 2021
+
+
+
+# Install packages and data
+install.packages("tidytuesdayR")
+library(tidytuesdayR)
+library(tidyverse)
+
+# Load datasets for the week of interest
+
+tuesdata <- tidytuesdayR::tt_load('2021-03-23')
+
+unvotes <- tuesdata$unvotes
+roll_calls <- tuesdata$roll_calls
+issues <- tuesdata$issues
+
+# initial observations
+
+## relational databases: which I haven't done yet!
+## need to do this module in R4DS before I can move on
