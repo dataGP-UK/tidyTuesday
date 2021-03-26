e9698250809e3c1252b724b50002eb694fabbfd9
dataGP-UK <sihulme21@gmail.com>
2021-03-26 18:36:33 +0000

    Reviewed relational tables to identify primary and foreign keys

--- 2021_03_23/tidyscript_2021_03_23.R
+++ 2021_03_23/tidyscript_2021_03_23.R
@@ -19,5 +19,12 @@ issues <- tuesdata$issues
 
 # initial observations
 
-## relational databases: which I haven't done yet!
-## need to do this module in R4DS before I can move on
+## relational databases - explore databases to identify keys
+
+roll_calls %>% count(rcid) %>% 
+  filter(n > 1) %>%   # rcid = primary key for roll_calls
+  nrow()
+
+## reviewed 3 datasets - roll_call rcid is primary key
+## no primary key in other data but roll_call$rcid as foreign key
+## that links each data base with roll_call
