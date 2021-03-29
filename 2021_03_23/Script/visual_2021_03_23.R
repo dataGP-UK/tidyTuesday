48742068ab48ad160a8851141981d570f67a35b5
dataGP-UK <sihulme21@gmail.com>
2021-03-29 10:16:35 +0100

    Created first visualisation

--- 2021_03_23/Script/visual_2021_03_23.R
+++ 2021_03_23/Script/visual_2021_03_23.R
@@ -6,11 +6,19 @@ issue_voting <- read_csv('issue_voting.csv')
 
 # visualisation
 
-issue_voting1 %>%
+issue_voting %>%
   filter(country %in% c('United States', 'Israel', 
-                        'United Kingdom', 'Saudi Arabia')) %>% 
-  ggplot(aes(country, yes))+
-  geom_col(aes(fill = issue))+
-  coord_flip()
-facet_wrap(~issue)+
+                        'United Kingdom')) %>%
+  
+  group_by(issue,country) %>% 
+  mutate(YES =
+           (yes / (yes + no + abstain) * 100), 
+         na.rm = TRUE) %>% 
+  ggplot(aes(country, YES))+
+  geom_col(aes(fill = country))+
+  coord_flip()+
+  facet_wrap(~issue)+
+  theme_bw()+
   theme(legend.position = 'NULL')
+  
+ 
