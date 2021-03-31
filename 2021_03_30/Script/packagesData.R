f2ce09366ae821dccff6fc4db64c6bda9d2eddb3
dataGP-UK <sihulme21@gmail.com>
2021-03-31 11:01:56 +0100

    updated to include master dataframe I created

--- 2021_03_30/Script/packagesData.R
+++ 2021_03_30/Script/packagesData.R
@@ -5,3 +5,6 @@ ulta <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidyt
 allCategories <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-30/allCategories.csv')
 allShades <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-30/allShades.csv')
 allNumbers <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-30/allNumbers.csv')
+
+master <-  read_csv('~/R/Repositories/tidyTuesday/2021_03_30/Data/master.csv')
+
