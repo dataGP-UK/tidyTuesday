a32e001184f4b60f6676c722a41f6231ef7d52ae
dataGP-UK <sihulme21@gmail.com>
2021-03-31 11:56:26 +0100

    deleted code as no unique variables found. not sure how data frames relate to each other will try looking and sephora and ulta instead

--- 2021_03_30/Script/tidying.R
+++ 2021_03_30/Script/tidying.R
@@ -1,24 +1,11 @@
 # how does each datatable relate to the others
 ## which is the master and what are the primary and foreign keys in each
 
-### allShades
+# deleted previous code - no unique identifiing variables found in
+## allNumbers, allShades, allCategories
+### only if uses imgSrc as a variable - duplicate images for same
+### products
 
-allShades %>% 
-  group_by(brand, product, description, imgSrc) %>% 
-  count() %>% 
-  filter(n == 1) %>% 
-  view()
 
-### this group of variables = primary key. there a duplicates
-### for (brand, product, description) due to multiple image urls for 
-### same product 
 
-# create master table for brand, product, description by selecting relevant
-# columns and filtering for distinct/unique observations
-# test this against other dataframes to check relationship to foreign keys
 
-master <- allShades %>% 
-  select(brand, product, description) %>% 
-  distinct()
-
-write_csv(master, 'master.csv')
