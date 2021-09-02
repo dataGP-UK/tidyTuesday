d7c0cb7c9d2ee9245ce040089cf99997e77a8e82
dataGP-UK <81358756+dataGP-UK@users.noreply.github.com>
2021-09-02 10:17:18 +0100

    tidy up repo

--- 2021_03_30/Script/packagesData.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-
-sephora <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-30/sephora.csv')
-ulta <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-30/ulta.csv')
-allCategories <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-30/allCategories.csv')
-allShades <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-30/allShades.csv')
-allNumbers <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-30/allNumbers.csv')
-
-
